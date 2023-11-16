21fb94f376fc203aa82f366eb7aa55e53664e6b7
ananth <ananthakrishnakongara@gmail.com>
2023-11-16 13:14:24 +0530

    adding return

--- stringReversal.js
+++ stringReversal.js
@@ -2,7 +2,7 @@ const stringReversal = (str) => {
 //let str = "aganitha";
 //let arr = str.split('');
 //arr.reverse();
-[...str].reduce((reversed, character) => {
+    return [...str].reduce((reversed, character) => {
     //console.log(character+reversed);
     return character + reversed;}, "");
 };
